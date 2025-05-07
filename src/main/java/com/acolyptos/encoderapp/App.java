bb16c4e30eb981691c6cf2429d8c397e8065dfda
JAVV <vinas.johnandrei@gmail.com>
2025-05-07 14:28:47 +0800

    init

--- src/main/java/com/acolyptos/encoderapp/App.java
+++ src/main/java/com/acolyptos/encoderapp/App.java
@@ -1,32 +1,11 @@
 package com.acolyptos.encoderapp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
-  private final static Dotenv DOTENV = Dotenv.load();
-  private final static String URL = DOTENV.get("DATABASE_URL");
-  private final static String USER = DOTENV.get("DATABASE_USER");
-  private final static String PASSWORD = DOTENV.get("DATABASE_PASSWORD"); 
-
-  private static Connection connection;
-
-  public static void main(String[] args) throws SQLException {
-    try {
-      Class.forName("org.postgresql.Driver");
-      connection = DriverManager.getConnection(URL, USER, PASSWORD);
-      System.out.println("Connected to PostgreSQL successfully.");
-
-    } catch (SQLException exception) {
-      System.out.println("Connection failed. " + exception.getMessage());
-      exception.printStackTrace();
-    } catch (ClassNotFoundException exception) {
-      System.out.println("PostgreSQL Driver not found.");
-      exception.printStackTrace();
-    } finally {
-      connection.close();
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);  
   }
 }
