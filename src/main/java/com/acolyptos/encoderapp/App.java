137d71613db09125f5a09c0ee8fb4042273ca33b
JAVV <vinas.johnandrei@gmail.com>
2025-05-07 12:59:50 +0800

    tested connection to PostgreSQL

--- src/main/java/com/acolyptos/encoderapp/App.java
+++ src/main/java/com/acolyptos/encoderapp/App.java
@@ -1,13 +1,36 @@
 package com.acolyptos.encoderapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+  private final static Dotenv DOTENV = Dotenv.load();
+  private final static String URL = DOTENV.get("DATABASE_URL");
+  private final static String USER = DOTENV.get("DATABASE_USER");
+  private final static String PASSWORD = DOTENV.get("DATABASE_PASSWORD"); 
+
+  private static Connection connection;
+
+  public static void main(String[] args) throws SQLException {
+    try {
+      Class.forName("org.postgresql.Driver");
+      connection = DriverManager.getConnection(URL, USER, PASSWORD);
+      System.out.println("Connected to PostgreSQL successfully.");
+
+    } catch (SQLException exception) {
+      System.out.println("Connection failed. " + exception.getMessage());
+      exception.printStackTrace();
+    } catch (ClassNotFoundException exception) {
+      System.out.println("PostgreSQL Driver not found.");
+      exception.printStackTrace();
+    } finally {
+      connection.close();
     }
+  }
 }
