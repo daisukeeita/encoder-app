46ed5b43dc5b5ffd9db26da7e2174a574d9bbf4b
JAVV <vinas.johnandrei@gmail.com>
2025-05-13 11:20:38 +0800

    handles missing input errors

---
+++ src/main/java/com/acolyptos/encoderapp/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package com.acolyptos.encoderapp.exceptions;
+
+public class ValidationException extends ApplicationException{
+  public ValidationException (String message) {
+    super(message);
+  }
+}
