e68bad8b3efba3334a57572b73a6992f1eecc4c1
JAVV <vinas.johnandrei@gmail.com>
2025-08-25 18:25:25 +0800

    added initial time creation and update

--- src/main/java/com/acolyptos/encoderapp/models/Vehicle.java
+++ src/main/java/com/acolyptos/encoderapp/models/Vehicle.java
@@ -1,19 +1,23 @@
 package com.acolyptos.encoderapp.models;
 
 import java.time.OffsetTime;
+import org.hibernate.annotations.CreationTimestamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "vehicle")
 public class Vehicle {
@@ -21,16 +25,20 @@ public class Vehicle {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-
+  
+  @NonNull
   @Column(nullable = false, length = 100)
   private String make;
 
+  @NonNull
   @Column(nullable = false, length = 100)
   private String model;
 
+  @NonNull
   @Column(nullable = false)
   private Integer year;
 
+  @NonNull
   @Column(nullable = false, length = 20, unique = true)
   private String license_plate;
 
@@ -40,10 +48,24 @@ public class Vehicle {
   @Column(unique = true, length = 20)
   private String mv_file_number;
 
+  // TODO: Update the time and date 
   @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMPZ DEFAULT NOW()")
   private OffsetTime created_at;
 
   @Column(name = "updated_at", nullable = false, columnDefinition = "TIMESTAMPZ DEFAULT NOW()")
+  @CreationTimestamp
   private OffsetTime updated_at;
+
+
+  @PrePersist
+  protected void onCreate() {
+    this.created_at = OffsetTime.now();
+    this.updated_at = OffsetTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    this.updated_at = OffsetTime.now();
+  }
 }
 
