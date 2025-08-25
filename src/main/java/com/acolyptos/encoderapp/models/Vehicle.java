6e18e13cd44914f5f0a6de13b0b7c8dfdc76f2a3
JAVV <vinas.johnandrei@gmail.com>
2025-08-25 14:29:44 +0800

    imported lombok to reduce boilerplate

--- src/main/java/com/acolyptos/encoderapp/models/Vehicle.java
+++ src/main/java/com/acolyptos/encoderapp/models/Vehicle.java
@@ -7,7 +7,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vehicle")
 public class Vehicle {
