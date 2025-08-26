68415af9e50e42bf8d5dd789614d6511e1ab599e
JAVV <vinas.johnandrei@gmail.com>
2025-08-26 17:11:39 +0800

    init

--- src/main/java/com/acolyptos/encoderapp/repositories/VehicleRepository.java
+++ src/main/java/com/acolyptos/encoderapp/repositories/VehicleRepository.java
@@ -1,9 +1,12 @@
 package com.acolyptos.encoderapp.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.acolyptos.encoderapp.models.Vehicle;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
-  List<Vehicle> findByMake(String make);
+  Optional<Vehicle> findVehicleByMake(String make);
 }
