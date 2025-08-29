454c6c52c09ac639466eb0b9bad72a40216f19d4
JAVV <vinas.johnandrei@gmail.com>
2025-08-29 14:26:54 +0800

    added multiple properties

--- src/main/java/com/acolyptos/encoderapp/models/Vehicle.java
+++ src/main/java/com/acolyptos/encoderapp/models/Vehicle.java
@@ -24,12 +24,65 @@ public class Vehicle {
   private long id;
 
   @NonNull
-  @Column(nullable = false, length = 100)
-  private String make;
+  @NotBlank(message = "Plate Number is required.")
+  @Column(name = "License_Plate", nullable = false, unique = true, length = 20)
+  private String licensePlate;
 
   @NonNull
-  @Column(nullable = false, length = 100)
+  @NotBlank(message = "Inspection ID is required.")
+  @Column(name = "Inspection_ID", nullable = false, unique = true, length = 100)
+  private String inspectionId;
+
+  @NonNull
+  @NotBlank(message = "Chassis Number is required.")
+  @Column(name = "Chassis", nullable = false, unique = true, length = 32)
+  private String chassis;
+
+  @NonNull
+  @NotBlank(message = "Engine Number is required.")
+  @Column(name = "Engine", nullable = false, unique = true, length = 20)
+  private String engine;
+
+  @NonNull
+  @NotBlank(message = "MV File Number is required.")
+  @Column(name = "MV_File_Number", nullable = false, unique = true, length = 20)
+  private String mvFileNumber;
+
+  @NonNull
+  @NotBlank(message = "Vehicle Color is required.")
+  @Column(name = "Color", nullable =  false, length = 20)
+  private String color;
+
+  @NonNull
+  @NotBlank(message = "Category Type is required.")
+  @Column(name = "Category_Type", nullable = false, length = 20)
+  private String categoryType;
+
+
+  @NonNull
+  @NotBlank(message = "Vehicle Model is required.")
+  @Column(name = "Model", nullable = false, length = 100)
   private String model;
 
+  @NonNull
+  @NotBlank(message = "Vehicle Model Year is required.")
+  @Column(name = "Model_Year", nullable = false, length = 5)
+  private String modelYear;
+
+  @NonNull
+  @NotBlank(message = "Fuel Type is required.")
+  @Column(name = "Fuel_Type", nullable = false, length = 7)
+  private String fuelType;
+
+  @Override
+  public String toString() {
+    return "Vehicle {\n\tlicensePlate: " + licensePlate + ",\n\tinspectionId: " + inspectionId
+        + ",\n\tchassis: " + chassis + ",\n\tengine: " + engine + ",\n\tmvFileNumber: "
+        + mvFileNumber + ",\n\tcolor: " + color + ",\n\tcategoryType: " + categoryType
+        + ",\n\tmodel: " + model + ",\n\tmodelYear: " + modelYear + ",\n\tfuelType: " + fuelType
+        + "\n}";
+  }
+
+  
 }
 
