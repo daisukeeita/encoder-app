49e364c756d28b49f56958b3fce8024c839c1a6c
JAVV <vinas.johnandrei@gmail.com>
2025-09-01 21:08:25 +0800

    comment for now

--- src/main/java/com/acolyptos/encoderapp/controllers/VehicleController.java
+++ src/main/java/com/acolyptos/encoderapp/controllers/VehicleController.java
@@ -1,38 +1,38 @@
-package com.acolyptos.encoderapp.controllers;
-
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.acolyptos.encoderapp.models.Vehicle;
-import com.acolyptos.encoderapp.services.VehicleService;
-import jakarta.validation.Valid;
-
-@RestController
-@RequestMapping(value = "api/v1/vehicles")
-public class VehicleController {
-
-  private final VehicleService vehicleService;
-
-  @Autowired
-  public VehicleController(VehicleService vehicleService) {
-    this.vehicleService = vehicleService;
-  }
-
-  @GetMapping("/getVehicleByPlateNumber/{plateNumber}")
-  public Optional<Vehicle> getVehicleByPlateNumber(@Valid @PathVariable String licensePlate) {
-    return vehicleService.getVehicleByLicensePlate(licensePlate);
-  }
-
-  @GetMapping("/getVehicleByChassisNumber/{chassisNumber}")
-  public Optional<Vehicle> getVehicleByChassisNumber(@Valid @PathVariable String chassis) {
-    return vehicleService.getVehicleByChassis(chassis);
-  }
-
-  @GetMapping("/getVehicleByMvFileNumber/{mvFileNumber}")
-  public Optional< Vehicle > getVehicleByMvFileNumber(@Valid @PathVariable String mvFileNumber) {
-    return vehicleService.getVehicleByMvFileNumber(mvFileNumber);
-  }
-}
+// package com.acolyptos.encoderapp.controllers;
+//
+// import java.util.Optional;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import com.acolyptos.encoderapp.models.Vehicle;
+// import com.acolyptos.encoderapp.services.VehicleService;
+// import jakarta.validation.Valid;
+//
+// @RestController
+// @RequestMapping(value = "api/v1/vehicles")
+// public class VehicleController {
+//
+//   private final VehicleService vehicleService;
+//
+//   @Autowired
+//   public VehicleController(VehicleService vehicleService) {
+//     this.vehicleService = vehicleService;
+//   }
+//
+//   @GetMapping("/getVehicleByPlateNumber/{plateNumber}")
+//   public Optional<Vehicle> getVehicleByPlateNumber(@Valid @PathVariable String licensePlate) {
+//     return vehicleService.getVehicleByLicensePlate(licensePlate);
+//   }
+//
+//   @GetMapping("/getVehicleByChassisNumber/{chassisNumber}")
+//   public Optional<Vehicle> getVehicleByChassisNumber(@Valid @PathVariable String chassis) {
+//     return vehicleService.getVehicleByChassis(chassis);
+//   }
+//
+//   @GetMapping("/getVehicleByMvFileNumber/{mvFileNumber}")
+//   public Optional< Vehicle > getVehicleByMvFileNumber(@Valid @PathVariable String mvFileNumber) {
+//     return vehicleService.getVehicleByMvFileNumber(mvFileNumber);
+//   }
+// }
