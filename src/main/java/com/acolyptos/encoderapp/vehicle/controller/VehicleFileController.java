eae095b5c05c8eb9cf1b9fb5a4817191a70c4c28
JAVV <vinas.johnandrei@gmail.com>
2025-09-02 14:02:18 +0800

    correcting imports

--- src/main/java/com/acolyptos/encoderapp/vehicle/controller/VehicleFileController.java
+++ src/main/java/com/acolyptos/encoderapp/vehicle/controller/VehicleFileController.java
@@ -1,4 +1,4 @@
-package com.acolyptos.encoderapp.controllers;
+package com.acolyptos.encoderapp.vehicle.controller;
 
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.acolyptos.encoderapp.models.Vehicle;
-import com.acolyptos.encoderapp.services.VehicleFileService;
+import com.acolyptos.encoderapp.vehicle.model.Vehicle;
+import com.acolyptos.encoderapp.vehicle.service.VehicleFileService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
