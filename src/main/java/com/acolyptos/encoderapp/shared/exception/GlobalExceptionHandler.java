901e366af3f05f77bc16fe5e1514bbf1d6be739c
JAVV <vinas.johnandrei@gmail.com>
2025-09-02 14:01:04 +0800

    global exception handler feature

---
+++ src/main/java/com/acolyptos/encoderapp/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.acolyptos.encoderapp.shared.exception;
+
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.acolyptos.encoderapp.vehicle.exception.VehicleDataNotFoundException;
+import com.acolyptos.encoderapp.vehicle.exception.VehicleDataParseException;
+import com.acolyptos.encoderapp.vehicle.exception.VehicleNotFoundException;
+
+/**
+ * Centralized exception handling for layers.
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(VehicleDataNotFoundException.class)
+  public ResponseEntity<Map<String, String>> handleDataNotFound(
+      final VehicleDataNotFoundException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(Map.of("error", exception.getMessage()));
+  }
+
+  @ExceptionHandler(VehicleDataParseException.class)
+  public ResponseEntity<Map<String, String>> handleDataParse(
+      final VehicleDataParseException exception) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Map.of("error", exception.getMessage()));
+  }
+
+  @ExceptionHandler(VehicleNotFoundException.class)
+  public ResponseEntity<Map<String, String>> handleVehicleNotFound(
+      final VehicleNotFoundException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(Map.of("error", exception.getMessage()));
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Map<String, String>> handleGeneral(final Exception exception) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .body(Map.of("error", "Unexpected error occured - ", "Details:", exception.getMessage()));
+  }
+}
