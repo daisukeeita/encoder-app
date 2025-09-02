70a670d239ed73e2074f5de072c42cef316a12e9
JAVV <vinas.johnandrei@gmail.com>
2025-09-02 14:02:42 +0800

    exception classes

---
+++ src/main/java/com/acolyptos/encoderapp/vehicle/exception/VehicleDataNotFoundException.java
@@ -0,0 +1,16 @@
+package com.acolyptos.encoderapp.vehicle.exception;
+
+/**
+ * Class Exception for Repository Layer.
+ */
+public class VehicleDataNotFoundException extends VehicleException {
+
+  /**
+   * Constructor of the class.
+   */
+  public VehicleDataNotFoundException(String message) {
+
+    super(message);
+  }
+
+}
---
+++ src/main/java/com/acolyptos/encoderapp/vehicle/exception/VehicleDataParseException.java
@@ -0,0 +1,16 @@
+package com.acolyptos.encoderapp.vehicle.exception;
+
+/**
+ * Class Exception for Bad Request.
+ */
+public class VehicleDataParseException extends VehicleException {
+
+  /**
+   * Constructor of the class.
+   */
+  public VehicleDataParseException(String message, Throwable cause) {
+
+    super(message, cause);
+  }
+
+}
