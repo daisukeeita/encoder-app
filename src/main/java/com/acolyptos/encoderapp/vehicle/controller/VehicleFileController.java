2a9d4bbb766b6d26003b10a07f154d3608e2fcb8
JAVV <vinas.johnandrei@gmail.com>
2025-09-03 13:39:39 +0800

    simplified the method due to redundancy

--- src/main/java/com/acolyptos/encoderapp/vehicle/controller/VehicleFileController.java
+++ src/main/java/com/acolyptos/encoderapp/vehicle/controller/VehicleFileController.java
@@ -2,37 +2,26 @@ package com.acolyptos.encoderapp.vehicle.controller;
 
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.acolyptos.encoderapp.vehicle.model.Vehicle;
 import com.acolyptos.encoderapp.vehicle.service.VehicleFileService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping(path = "/mock/vehicleInformation", produces = MediaType.APPLICATION_JSON_VALUE)
 public class VehicleFileController {
 
   private final VehicleFileService vehicleFileService;
-  private final ObjectMapper objectMapper = new ObjectMapper();
 
   @Autowired
-  public VehicleFileController (VehicleFileService vehicleFileService) {
+  public VehicleFileController(VehicleFileService vehicleFileService) {
     this.vehicleFileService = vehicleFileService;
   }
 
   @GetMapping("/v1")
-  public ResponseEntity<Vehicle> getVehicleInformation() throws IOException {
-    try {
-      Vehicle vehicle = vehicleFileService.filterVehicleInformationFromJson();
-
-      return ResponseEntity.ok(vehicle);
-
-    } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    }
+  public Vehicle getVehicleInformation() throws IOException {
+    return vehicleFileService.setFilterVehicleInformationFromJson();
   }
 }
