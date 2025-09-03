6c339843adb8de1f52a3c93b07860e317b5f9f65
JAVV <vinas.johnandrei@gmail.com>
2025-09-03 13:40:24 +0800

    explicitly add comments and exception handling

--- src/main/java/com/acolyptos/encoderapp/vehicle/repository/VehicleFileRepository.java
+++ src/main/java/com/acolyptos/encoderapp/vehicle/repository/VehicleFileRepository.java
@@ -1,27 +1,41 @@
 package com.acolyptos.encoderapp.vehicle.repository;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
+import com.acolyptos.encoderapp.vehicle.exception.VehicleDataParseException;
 import com.acolyptos.encoderapp.vehicle.model.VehicleInspection;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Repository
 public class VehicleFileRepository {
 
   private final ObjectMapper objectMapper = new ObjectMapper();
+  private final ClassPathResource resource = new ClassPathResource("mock/vehicle.json");
 
-  public VehicleInspection getVehicleFromJson() throws IOException {
-    try {
-      ClassPathResource resource = new ClassPathResource("mock/vehicle.json");
-      VehicleInspection vehicleInspection = objectMapper.readValue(
-        resource.getInputStream().readAllBytes(), 
-        VehicleInspection.class
-      );
+  /**
+   * Fetches and return a vehicle information or throws an error if there's no data.
+   *
+   * @return VehicleInspection object once the data is fetched.
+   *
+   * @throws IOException if it the file is empty or the data is missing.
+   */
+  public VehicleInspection fetchVehicleData() throws IOException {
+    try (InputStream file = resource.getInputStream()) {
+      final VehicleInspection vehicleInspectionData =
+          objectMapper.readValue(file, VehicleInspection.class);
 
-      return vehicleInspection;
-    } catch (IOException e) {
-      throw new IOException(e.getMessage());
+      return vehicleInspectionData;
+    } catch (final FileNotFoundException exception) {
+      throw new VehicleDataParseException("Vehicle data file not found: ", exception);
+    } catch (final JsonProcessingException exception) {
+      throw new VehicleDataParseException("Invalid vehicle data file: ", exception);
+    } catch (final IOException exception) {
+      throw new VehicleDataParseException("I/O error while reading vehicle data: ", exception);
     }
+
   }
 }
