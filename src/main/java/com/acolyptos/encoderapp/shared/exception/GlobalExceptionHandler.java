d36c57fb01b36949dba8bd8e0e0642d8b4512ae6
JAVV <vinas.johnandrei@gmail.com>
2025-09-04 11:39:57 +0800

    replaced to centralized repository file exception handler

--- src/main/java/com/acolyptos/encoderapp/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/com/acolyptos/encoderapp/shared/exception/GlobalExceptionHandler.java
@@ -15,9 +15,33 @@ import com.acolyptos.encoderapp.vehicle.exception.VehicleNotFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+  /**
+   * Handle exceptions in vehicle file handling (repository layer).
+   *
+   * @param exception exception object that will be passed
+   * @return an error reponse object as Reponse Entity
+   */
+  @ExceptionHandler(VehicleFileHandlingException.class)
+  public ResponseEntity<ErrorResponse> handleVehicleFileExceptions(
+      final VehicleFileHandlingException exception) {
 
+    final Throwable cause = exception.getCause();
+    final HttpStatus status = mapExceptionsToHttpStatus(cause);
+    final ErrorResponse errorResponse = new ErrorResponse(status.value(), exception.getMessage());
 
+    return ResponseEntity.status(status).body(errorResponse);
   }
 
+  private HttpStatus mapExceptionsToHttpStatus(final Throwable cause) {
+    if (cause instanceof FileNotFoundException) {
+      return HttpStatus.NOT_FOUND;
+    } else if (cause instanceof EOFException) {
+      return HttpStatus.BAD_REQUEST;
+    } else if (cause instanceof InterruptedIOException) {
+      return HttpStatus.REQUEST_TIMEOUT;
+    }
+
+    return HttpStatus.INTERNAL_SERVER_ERROR;
   }
+
 }
