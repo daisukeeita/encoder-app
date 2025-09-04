2d514b7e74cf1647bf586b80f153ebd3a87f3e51
JAVV <vinas.johnandrei@gmail.com>
2025-09-04 11:42:47 +0800

    init

--- src/main/java/com/acolyptos/encoderapp/App.java
+++ src/main/java/com/acolyptos/encoderapp/App.java
@@ -3,23 +3,10 @@ package com.acolyptos.encoderapp;
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
-import com.acolyptos.encoderapp.models.VehicleInspection;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class App {
   public static void main(String[] args) throws IOException {
     SpringApplication.run(App.class, args);
-
-    ObjectMapper mapper = new ObjectMapper();
-    ClassPathResource resource = new ClassPathResource("mock/vehicle.json");
-
-    VehicleInspection vehicle = mapper.readValue(
-        resource.getInputStream().readAllBytes(), 
-        VehicleInspection.class
-      );
-
-    System.out.println(vehicle.toString());
   }
 }
