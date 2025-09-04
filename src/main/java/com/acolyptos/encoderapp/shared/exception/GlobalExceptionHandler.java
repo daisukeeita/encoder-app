0cdcf0f296df56b9a54a322caa2914f1886da944
JAVV <vinas.johnandrei@gmail.com>
2025-09-04 11:41:07 +0800

    deleted unused imports, add IOException subclasses

--- src/main/java/com/acolyptos/encoderapp/shared/exception/GlobalExceptionHandler.java
+++ src/main/java/com/acolyptos/encoderapp/shared/exception/GlobalExceptionHandler.java
@@ -1,13 +1,13 @@
 package com.acolyptos.encoderapp.shared.exception;
 
-import java.util.Map;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.InterruptedIOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import com.acolyptos.encoderapp.vehicle.exception.VehicleDataNotFoundException;
-import com.acolyptos.encoderapp.vehicle.exception.VehicleDataParseException;
-import com.acolyptos.encoderapp.vehicle.exception.VehicleNotFoundException;
+import com.acolyptos.encoderapp.vehicle.exception.VehicleFileHandlingException;
 
 /**
  * Centralized exception handling for layers.
