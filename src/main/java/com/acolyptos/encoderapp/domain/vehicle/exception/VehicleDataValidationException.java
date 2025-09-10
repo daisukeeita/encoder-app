f57a96463e60e6ca18a6557563af23a8331d96cc
JAVV <vinas.johnandrei@gmail.com>
2025-09-10 20:52:38 +0800

    modified package path

--- src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleDataValidationException.java
+++ src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleDataValidationException.java
@@ -1,4 +1,4 @@
-package com.acolyptos.encoderapp.vehicle.exception;
+package com.acolyptos.encoderapp.domain.vehicle.exception;
 
 /**
  * Custom class exception for Validating Data (Service Layer).
