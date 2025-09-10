148a27e94268adb3212fe1a855ecebc2abf98ba7
JAVV <vinas.johnandrei@gmail.com>
2025-09-10 20:55:09 +0800

    modified entry path for vehicle feature

--- src/main/java/com/acolyptos/encoderapp/interfaces/vehicle/VehicleController.java
+++ src/main/java/com/acolyptos/encoderapp/interfaces/vehicle/VehicleController.java
@@ -2,14 +2,16 @@ package com.acolyptos.encoderapp.interfaces.vehicle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.acolyptos.encoderapp.application.vehicle.VehicleAppService;
 import com.acolyptos.encoderapp.domain.vehicle.model.Vehicle;
+import com.acolyptos.encoderapp.domain.vehicle.model.VehicleRequest;
 
 @RestController
-@RequestMapping(path = "/api/v1/vehicleInformation", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("api/v1/encoder/")
 public class VehicleController {
 
   private final VehicleAppService vehicleAppService;
@@ -19,8 +21,11 @@ public class VehicleController {
     this.vehicleAppService = vehicleAppService;
   }
 
-  @GetMapping("/mock")
-  public Vehicle retrieveVehicleFromFile() throws Exception {
-    return vehicleAppService.filterVehicleInspectionFromJson();
+  @PostMapping(
+    value = "/requestVehicle",
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public Vehicle retrieveVehicleFromFile(@RequestBody VehicleRequest vehicleRequest) throws Exception {
+    return vehicleAppService.filterVehicleInspectionFromJson(vehicleRequest);
   }
 }
