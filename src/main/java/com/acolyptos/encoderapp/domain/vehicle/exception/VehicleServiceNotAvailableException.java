5b062ac773ff50af0eb4ad6e5091d410b20988bf
JAVV <vinas.johnandrei@gmail.com>
2025-09-16 20:59:42 +0800

    Added Service Not Available Exception class for calling Third-Party API

---
+++ src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleServiceNotAvailableException.java
@@ -0,0 +1,8 @@
+package com.acolyptos.encoderapp.domain.vehicle.exception;
+
+public class VehicleServiceNotAvailableException extends VehicleException {
+
+  public VehicleServiceNotAvailableException (final String message) {
+    super(message);
+  }
+}
