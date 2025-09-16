7b9cda715d5f92b737391457903df76257ad827e
JAVV <vinas.johnandrei@gmail.com>
2025-09-16 20:59:11 +0800

    Added Resource Not Found Exception class for data fetching from calling Third-Party API

---
+++ src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.acolyptos.encoderapp.domain.vehicle.exception;
+
+public class VehicleResourceNotFoundException extends VehicleException {
+
+  public VehicleResourceNotFoundException(final String message) {
+    super(message);
+  }
+}
