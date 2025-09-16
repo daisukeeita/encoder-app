4e22d4d2fa25f4343916d3e7e7036d133709981b
JAVV <vinas.johnandrei@gmail.com>
2025-09-16 20:58:12 +0800

    Deleted unused Exception Class

--- src/main/java/com/acolyptos/encoderapp/domain/exception/GlobalExceptionHandler.java
+++ src/main/java/com/acolyptos/encoderapp/domain/exception/GlobalExceptionHandler.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import com.acolyptos.encoderapp.domain.vehicle.exception.VehicleDataValidationException;
 import com.acolyptos.encoderapp.domain.vehicle.exception.VehicleFileHandlingException;
-import com.acolyptos.encoderapp.domain.vehicle.exception.VehicleMissingDataException;
+import com.acolyptos.encoderapp.domain.vehicle.exception.VehicleResourceNotFoundException;
+import com.acolyptos.encoderapp.domain.vehicle.exception.VehicleServiceNotAvailableException;
 
 /**
  * Centralized exception handling for layers.
@@ -18,7 +18,7 @@ import com.acolyptos.encoderapp.domain.vehicle.exception.VehicleMissingDataExcep
 public class GlobalExceptionHandler {
 
   /**
-   * Handle exceptions in vehicle file handling (repository layer).
+   * Handle exceptions in vehicle file handling (client layer).
    *
    * @param exception exception object that will be passed
    * @return an error reponse object as Reponse Entity
@@ -40,27 +40,37 @@ public class GlobalExceptionHandler {
    * @param exception exception object that will be passed
    * @return an error reponse object as Reponse Entity
    */
-  @ExceptionHandler(VehicleDataValidationException.class)
-  public ResponseEntity<ErrorResponse> handleVehicleValidationExceptions(
-      final VehicleDataValidationException exception) {
-
-    final Throwable cause = exception.getCause();
-    final HttpStatus status = mapExceptionsToHttpStatus(cause);
-    final ErrorResponse errorResponse = new ErrorResponse(status.value(), exception.getMessage());
-
-    return ResponseEntity.status(status).body(errorResponse);
+  @ExceptionHandler(VehicleResourceNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleVehicleResourceNotFoundException(
+      final VehicleResourceNotFoundException exception) {
+    final ErrorResponse errorResponse =
+        new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
   }
 
-  @ExceptionHandler(VehicleMissingDataException.class)
-  public ResponseEntity<ErrorResponse> handleMissingDataValidationException(
-      final VehicleMissingDataException exception) {
+  @ExceptionHandler(VehicleServiceNotAvailableException.class)
+  public ResponseEntity<ErrorResponse> handleVehicleServiceNotAvailableException(
+      final VehicleServiceNotAvailableException exception) {
     final ErrorResponse errorResponse =
-        new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        new ErrorResponse(HttpStatus.SERVICE_UNAVAILABLE.value(), exception.getMessage());
+    return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(errorResponse);
+  }
+
+  @ExceptionHandler(UnexpectedError.class)
+  public ResponseEntity<ErrorResponse> handleUnexpectederror(final UnexpectedError exception) {
+    final ErrorResponse errorResponse =
+        new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleAllExceptions(final Exception exception) {
+    final ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
   }
 
   private HttpStatus mapExceptionsToHttpStatus(final Throwable cause) {
-    if (cause instanceof FileNotFoundException || cause instanceof VehicleMissingDataException) {
+    if (cause instanceof FileNotFoundException) {
       return HttpStatus.NOT_FOUND;
     } else if (cause instanceof EOFException) {
       return HttpStatus.BAD_REQUEST;
