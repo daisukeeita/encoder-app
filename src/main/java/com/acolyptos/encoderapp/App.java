5237c98d131c0efd995f71979097449bdaf2e705
JAVV <vinas.johnandrei@gmail.com>
2025-09-17 13:32:20 +0800

    disable database connection

--- src/main/java/com/acolyptos/encoderapp/App.java
+++ src/main/java/com/acolyptos/encoderapp/App.java
@@ -2,9 +2,19 @@ package com.acolyptos.encoderapp;
 
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(
+    exclude = {
+      DataSourceAutoConfiguration.class,
+      DataSourceTransactionManagerAutoConfiguration.class,
+      HibernateJpaAutoConfiguration.class
+    })
 public class App {
   public static void main(String[] args) throws IOException {
     SpringApplication.run(App.class, args);
