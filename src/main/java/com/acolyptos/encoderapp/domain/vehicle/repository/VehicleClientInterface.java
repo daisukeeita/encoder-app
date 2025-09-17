d9ba49103d3791f8ca819b0bbd83b8ca2ed32bfa
JAVV <vinas.johnandrei@gmail.com>
2025-09-17 22:04:28 +0800

    added javadoc comment

--- src/main/java/com/acolyptos/encoderapp/domain/vehicle/repository/VehicleClientInterface.java
+++ src/main/java/com/acolyptos/encoderapp/domain/vehicle/repository/VehicleClientInterface.java
@@ -3,13 +3,15 @@ package com.acolyptos.encoderapp.domain.vehicle.repository;
 import com.acolyptos.encoderapp.domain.vehicle.model.VehicleInspection;
 import com.acolyptos.encoderapp.domain.vehicle.model.VehicleRequest;
 
-/**
- * Interface for fetching vehicle data.
- */
+/** A client interface for fetching raw vehicle data from an external source. */
 public interface VehicleClientInterface {
 
   /**
-   * Rule for this interface.
+   * Fetches raw vehicle data from either a file or an API.
+   *
+   * @param vehicleRequest The request containing the criteria used to find vehicle details.
+   * @return A {@link VehicleInspection} for the requested vehicle.
+   * @throws Exception if the vehicle data cannot be found or an error occurs during the fetch.
    */
   VehicleInspection fetchVehicleData(VehicleRequest vehicleRequest) throws Exception;
 }
