9096301901c4b8ed8efffc3d5978458c0309c357
JAVV <vinas.johnandrei@gmail.com>
2025-09-17 13:43:20 +0800

    added javadoc comment

--- src/main/java/com/acolyptos/encoderapp/interfaces/vehicle/VehicleController.java
+++ src/main/java/com/acolyptos/encoderapp/interfaces/vehicle/VehicleController.java
@@ -1,21 +1,30 @@
 package com.acolyptos.encoderapp.interfaces.vehicle;
 
+import com.acolyptos.encoderapp.application.vehicle.VehicleAppService;
+import com.acolyptos.encoderapp.domain.vehicle.model.Vehicle;
+import com.acolyptos.encoderapp.domain.vehicle.model.VehicleRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.acolyptos.encoderapp.application.vehicle.VehicleAppService;
-import com.acolyptos.encoderapp.domain.vehicle.model.Vehicle;
-import com.acolyptos.encoderapp.domain.vehicle.model.VehicleRequest;
 
+/**
+ * A Controller responsible for handling incoming client request to process the vehicle data once
+ * fetched from the external source and returns an appropriate response based on the result.
+ */
 @RestController
 @RequestMapping("api/v1/encoder/")
 public class VehicleController {
 
   private final VehicleAppService vehicleAppService;
 
+  /**
+   * Constructs a new VehicleController with necessary dependency.
+   *
+   * @param vehicleAppService The service that process raw data from the external source. object.
+   */
   @Autowired
   public VehicleController(final VehicleAppService vehicleAppService) {
     this.vehicleAppService = vehicleAppService;
