fb7719b4bea27bcd7726b2a2bbf293017d86f8f2
JAVV <vinas.johnandrei@gmail.com>
2025-09-17 11:19:36 +0800

    added javadoc comment

--- src/main/java/com/acolyptos/encoderapp/application/vehicle/VehicleAppService.java
+++ src/main/java/com/acolyptos/encoderapp/application/vehicle/VehicleAppService.java
@@ -1,8 +1,5 @@
 package com.acolyptos.encoderapp.application.vehicle;
 
-import java.util.Set;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.acolyptos.encoderapp.domain.exception.UnexpectedError;
 import com.acolyptos.encoderapp.domain.vehicle.model.Vehicle;
 import com.acolyptos.encoderapp.domain.vehicle.model.VehicleInspection;
@@ -12,21 +9,51 @@ import com.acolyptos.encoderapp.domain.vehicle.service.VehicleService;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+/**
+ * A Service responsible for handling vehicle-related business logic, including data fetching,
+ * validation, and processing.
+ */
 @Service
 public class VehicleAppService {
-  private final VehicleClientInterface vehicleClientInterface;
-  private final VehicleService vehicleService;
   private final Validator validator;
+  private final VehicleService vehicleService;
+  private final VehicleClientInterface vehicleClientInterface;
 
+  /**
+   * Constructs a new VehicleAppService with necessary dependencies.
+   *
+   * @param validator The validator used to check constraints on vehicle inspection data.
+   * @param vehicleService The service that processes {@link VehicleInspection} data into a {@link
+   *     Vehicle} Object.
+   * @param vehicleClientInterface The client for fetching raw vehicle data.
+   */
   @Autowired
-  public VehicleAppService(final Validator validator, final VehicleService vehicleService,
+  public VehicleAppService(
+      final Validator validator,
+      final VehicleService vehicleService,
       final VehicleClientInterface vehicleClientInterface) {
     this.validator = validator;
     this.vehicleService = vehicleService;
     this.vehicleClientInterface = vehicleClientInterface;
   }
 
+  /**
+   * Filters and validates a Vehicle Inspection from a JSON Request.
+   *
+   * <p>This method fetches raw vehicle data using the provided request, validates the retrieved
+   * inspection object, and then process it into a {@link Vehicle} object. If validation fails, it
+   * throws {@link ConstraintViolationException}. If an I/O error occurs during data fetching, it
+   * throws an {@link UnexpectedError}.
+   *
+   * @param vehicleRequest The request object containing vehicle information for fetching data.
+   * @return A {@link Vehicle} object processed from validated vehicle inspection.
+   * @throws UnexpectedError If an I/O or other unexpected issue occurs during data fetching.
+   * @throws ConstraintViolationException If the fetched data violates any validation constraints.
+   */
   public Vehicle filterVehicleInspectionFromJson(final VehicleRequest vehicleRequest)
       throws Exception {
 
@@ -47,5 +74,4 @@ public class VehicleAppService {
       throw new UnexpectedError("Unexpected I/O error occured: " + exception.getMessage());
     }
   }
-
 }
