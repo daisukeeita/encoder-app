a1e4fa13560a04851ff734ea35218f0ca46be1dc
JAVV <vinas.johnandrei@gmail.com>
2025-09-18 13:45:39 +0800

    added javadoc comment

--- src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleResourceNotFoundException.java
+++ src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleResourceNotFoundException.java
@@ -1,7 +1,12 @@
 package com.acolyptos.encoderapp.domain.vehicle.exception;
 
+/** The class responsible for handling the API's thrown vehicle data that not found error. */
 public class VehicleResourceNotFoundException extends VehicleException {
-
+  /**
+   * Constructs the class with description of the error.
+   *
+   * @param message The customized description of the error occured.
+   */
   public VehicleResourceNotFoundException(final String message) {
     super(message);
   }
