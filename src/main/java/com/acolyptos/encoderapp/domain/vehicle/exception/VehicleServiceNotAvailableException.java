a12e19046f0867e297c6a77c8b75f642b6ab1c85
JAVV <vinas.johnandrei@gmail.com>
2025-09-18 13:45:44 +0800

    added javadoc comment

--- src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleServiceNotAvailableException.java
+++ src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleServiceNotAvailableException.java
@@ -1,8 +1,13 @@
 package com.acolyptos.encoderapp.domain.vehicle.exception;
 
+/** The class responsible for handling the API's thrown service unavailable error. */
 public class VehicleServiceNotAvailableException extends VehicleException {
-
-  public VehicleServiceNotAvailableException (final String message) {
+  /**
+   * Constructs the class with description of the error.
+   *
+   * @param message The customized description of the error occured.
+   */
+  public VehicleServiceNotAvailableException(final String message) {
     super(message);
   }
 }
