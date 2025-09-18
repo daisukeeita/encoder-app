82086cee216b80610c78c78d8524fdd4069e515d
JAVV <vinas.johnandrei@gmail.com>
2025-09-18 13:45:33 +0800

    added javadoc comment

--- src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleFileHandlingException.java
+++ src/main/java/com/acolyptos/encoderapp/domain/vehicle/exception/VehicleFileHandlingException.java
@@ -1,24 +1,25 @@
 package com.acolyptos.encoderapp.domain.vehicle.exception;
 
 /**
- * Custom class exception for repository layer.
+ * The class responsible for handling the errors that will occur while fetching the raw data from a
+ * file.
  */
 public class VehicleFileHandlingException extends VehicleException {
 
   /**
-   * Class constructor of File Handling Exceptions.
+   * Constructs the class with description of the error.
    *
-   * @param message description of error occured.
+   * @param message The customized description of the error occured.
    */
   public VehicleFileHandlingException(final String message) {
     super(message);
   }
 
   /**
-   * Class constructor of File Handling Exceptions.
+   * Constructs the class with the description of the error and where it is coming from.
    *
-   * @param message description of error occured.
-   * @param cause source of error occured.
+   * @param message The customized description of the error occured.
+   * @param cause The source of the error occured.
    */
   public VehicleFileHandlingException(final String message, final Throwable cause) {
     super(message, cause);
