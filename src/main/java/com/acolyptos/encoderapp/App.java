41b412f9e29aa3a742dfabbd1ed52a73175f1a8a
JAVV <vinas.johnandrei@gmail.com>
2025-10-02 22:30:33 +0800

    init logging

--- src/main/java/com/acolyptos/encoderapp/App.java
+++ src/main/java/com/acolyptos/encoderapp/App.java
@@ -1,6 +1,8 @@
 package com.acolyptos.encoderapp;
 
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +18,10 @@ import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerA
       HibernateJpaAutoConfiguration.class
     })
 public class App {
+  private static final Logger serverLog = LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) throws IOException {
     SpringApplication.run(App.class, args);
+    serverLog.info("Encoder Application started.");
   }
 }
