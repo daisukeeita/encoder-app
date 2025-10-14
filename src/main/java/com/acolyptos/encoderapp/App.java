cddc7d046e7e2b6b29539677146cf69844748bec
JAVV <vinas.johnandrei@gmail.com>
2025-10-14 09:48:24 +0800

    enabling jpa connection

--- src/main/java/com/acolyptos/encoderapp/App.java
+++ src/main/java/com/acolyptos/encoderapp/App.java
@@ -1,26 +1,26 @@
 package com.acolyptos.encoderapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(
-    exclude = {
-      DataSourceAutoConfiguration.class,
-      DataSourceTransactionManagerAutoConfiguration.class,
-      HibernateJpaAutoConfiguration.class
-    })
+// @EnableAutoConfiguration(
+//     exclude = {
+//       DataSourceAutoConfiguration.class,
+//       DataSourceTransactionManagerAutoConfiguration.class,
+//       HibernateJpaAutoConfiguration.class
+//     })
 public class App {
   private static final Logger serverLog = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) throws IOException {
+    Dotenv dotenv = Dotenv.load();
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
     SpringApplication.run(App.class, args);
     serverLog.info("Encoder Application started.");
   }
